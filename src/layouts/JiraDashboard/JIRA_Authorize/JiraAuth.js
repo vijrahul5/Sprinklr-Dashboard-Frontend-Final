528910433cd9cbc717702fcdd37a4c2c321af60d
Rahul Vij <vijrahul5>
2021-07-04 12:43:09 +0530

    Jira URL changes

--- src/layouts/JiraDashboard/JIRA_Authorize/JiraAuth.js
+++ src/layouts/JiraDashboard/JIRA_Authorize/JiraAuth.js
@@ -8,7 +8,7 @@ const JiraAuth = () => {
   const { showAuthPage, getTokens } = useAuthorize();
   const [clientId, setClientId] = useState("");
   const [clientSecret, setClientSecret] = useState("");
-  let URL = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${clientId}&scope=offline_access%20read%3Ajira-user%20read%3Ajira-work%20manage%3Ajira-project%20manage%3Ajira-configuration%20write%3Ajira-work%20manage%3Ajira-webhook%20manage%3Ajira-data-provider&redirect_uri=http%3A%2F%2Flocalhost%3A3000&state=jiiaa&response_type=code&prompt=consent`;
+  let URL = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${clientId}&scope=read%3Ajira-user%20read%3Ajira-work%20manage%3Ajira-project%20manage%3Ajira-configuration%20write%3Ajira-work%20manage%3Ajira-webhook%20manage%3Ajira-data-provider&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdashboard&state=jiiaa&response_type=code&prompt=consent`;
   console.log(clientId);
   console.log(clientSecret);
   return (
